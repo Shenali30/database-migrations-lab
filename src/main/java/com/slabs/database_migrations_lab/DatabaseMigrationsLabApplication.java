3105cbcd27bcf34604791222104d6ab7ffbf8e73
shenali30 <shenalik30@gmail.com>
2025-12-15 14:20:08 +0530

    Setup Spring Boot

---
+++ src/main/java/com/slabs/database_migrations_lab/DatabaseMigrationsLabApplication.java
@@ -0,0 +1,13 @@
+package com.slabs.database_migrations_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMigrationsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseMigrationsLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slabs/database_migrations_lab/DatabaseMigrationsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slabs.database_migrations_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseMigrationsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
